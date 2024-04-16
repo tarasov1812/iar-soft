f8d5c22632b1f0709a989b6fc8fd67a430898718
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-17 00:11:18 +0200

    x

--- app.mjs
+++ app.mjs
@@ -7,6 +7,10 @@ const port = 3000;
 // const index = fs.readFileSync('index.html', 'utf8');
 // app.get('/', (req, res) => res.type('html').send(index));
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(bodyParser.json());
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
