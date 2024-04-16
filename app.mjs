18388dc74e1d3ec3220eebc0cfca856e9904cb99
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-17 00:09:39 +0200

    z

--- app.mjs
+++ app.mjs
@@ -4,8 +4,8 @@ import fs from 'fs';
 const app = express();
 const port = 3000;
 
-const index = fs.readFileSync('public/index.html', 'utf8');
-app.get('/', (req, res) => res.type('html').send(index));
+// const index = fs.readFileSync('index.html', 'utf8');
+// app.get('/', (req, res) => res.type('html').send(index));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
