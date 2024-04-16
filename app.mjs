ff189542ff09fb7ac8d39b9bfad2ee2209b5e3d4
Alexandr Tarasov <75462526+tarasov1812@users.noreply.github.com>
2024-04-17 00:24:07 +0200

    oip

--- app.mjs
+++ app.mjs
@@ -4,9 +4,6 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const index = fs.readFileSync('index.html', 'utf8');
-// app.get('/', (req, res) => res.type('html').send(index));
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(bodyParser.json());
